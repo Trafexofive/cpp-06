5c11fa56a1b8c9b03263c29158e0f7acf6b81c35
your_name <your_email@example.com>
2025-02-19 15:30:59 +0100

    latest

--- run.sh
+++ run.sh
@@ -3,6 +3,27 @@
 # Get the folder number from argument
 num=$1
 
+#if arg is clean then clean all
+
+if [ "$num" == "clean" ]; then
+    for i in {0..9}; do
+        cd ex0$i || continue
+        make clean
+        cd ..
+    done
+    exit 0
+fi
+
+
+if [ "$num" == "fclean" ]; then
+    for i in {0..9}; do
+        cd ex0$i || continue
+        make fclean
+        cd ..
+    done
+    exit 0
+fi
+
 # Validate input
 if [[ ! $num =~ ^[0-9]+$ ]] || (( num < 0 || num > 9 )); then
     echo "Usage: ./make-run [0-9]"
